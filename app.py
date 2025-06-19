6618a669b67909e73962a3f8f857b272161f5bfa
ErkiSture <starmine04@gmail.com>
2025-06-19 18:34:28 +0200

    setup virtual enviroment

--- app.py
+++ app.py
@@ -12,15 +12,11 @@ def get_db():
         g.db.row_factory = sqlite3.Row
     return g.db
 
-
-
 @app.teardown_appcontext
 def close_db(exception):
     db = g.pop('db', None)
     if db is not None:
         db.close()
-
-
 
 def init_db():
     db = sqlite3.connect(DATABASE)
@@ -111,12 +107,6 @@ def logout():
     session.clear()
     return redirect(url_for('login'))
 
-
 if __name__ == '__main__':
     init_db()
     app.run(debug=False)
-
-
-
-
-#this is my kommentar
